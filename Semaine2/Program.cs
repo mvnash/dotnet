7b8b00d37ed55fa97548f9d9bdd4e9a1cf57ab7a
Maxime Vlaminck <maxime.vlaminck@student.vinci.be>
2023-09-20 12:46:57 +0200

    fin seance 2

---
+++ Semaine2/Program.cs
@@ -0,0 +1,33 @@
+﻿using LINQDataContext;
+
+DataContext dc = new DataContext();
+
+/*
+Student? jdepp = (from student in dc.Students
+                  where student.Login == "jdepp"
+                  select student).SingleOrDefault();
+
+if (jdepp != null)
+{
+    Console.WriteLine(jdepp.Last_Name + jdepp.First_Name);
+}
+
+IEnumerable<Student> students = (from student in dc.Students
+                      select student);
+
+foreach (Student student in students)
+{
+    Console.WriteLine(student.Last_Name + " " + student.First_Name + " : " + student.Student_ID + " "  + student.BirthDate);
+}
+*/
+
+
+IEnumerable<Student> students = (from student in dc.Students
+                                 where student.BirthDate.Year < 1995
+                                 select student);
+
+foreach (Student student in students)
+{
+    Console.WriteLine(student.Last_Name + " " + student.First_Name + " : " + student.Year_Result + " " + student.Year_Result>=12);
+}
+
