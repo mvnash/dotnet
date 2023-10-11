5ce92537561896262f6b7b9b79133b95d4e83f3a
Maxime Vlaminck <maxime.vlaminck@student.vinci.be>
2023-10-11 12:53:27 +0200

    a

--- semaine3/semaine3/semaine3/Models/NorthwindContext.cs
+++ semaine3/semaine3/semaine3/Models/NorthwindContext.cs
@@ -70,16 +70,16 @@ public partial class NorthwindContext : DbContext
     public virtual DbSet<Territory> Territories { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
     {
         if (!optionsBuilder.IsConfigured)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial 			Catalog=Northwind;MultipleActiveResultSets=True")
-                      .UseLazyLoadingProxies()
-                      .LogTo(Console.WriteLine, LogLevel.Information)
-                      .EnableSensitiveDataLogging();
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Northwind;MultipleActiveResultSets=True")
+                     .UseLazyLoadingProxies();
+                      //.LogTo(Console.WriteLine, LogLevel.Information)
+                      //.EnableSensitiveDataLogging();
         }
-    };
+    }
+
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
--- semaine3/semaine3/semaine3/Program.cs
+++ semaine3/semaine3/semaine3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using semaine3.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        NorthwindContext context = new NorthwindContext();
+
+        Console.WriteLine("Entrez une ville.");
+        string ville = Console.ReadLine();
+
+        if (ville == null) return;
+
+        Customer[] cust = (from c in context.Customers
+                           where c.City == ville
+                           select c).ToArray();
+    }
+}
