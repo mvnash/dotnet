2c827d9841d0db6d7224032bf8ce8c368f296a5b
Vlaminck Maxime <maxime.vlaminck@outlook.com>
2024-01-11 22:18:58 +0100

    j2023

---
+++ examjanvier2023/question1/question1/question1/ViewModels/ProductModel.cs
@@ -0,0 +1,55 @@
+﻿using question1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question1.ViewModels
+{
+    class ProductModel : INotifyPropertyChanged
+    {
+        private readonly Product _product;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+            }
+        }
+
+        public ProductModel(Product product)
+        {
+            _product = product;
+        }
+
+        public int ProductId
+        {
+            get { return _product.ProductId; }
+        }
+
+        public string ProductName
+        {
+            get { return _product.ProductName; }
+            set { _product.ProductName = value; OnPropertyChanged("ProductInfo"); }
+        }
+
+        public string ProductInfo
+        {
+            get { return ProductId + ProductName; }
+        }
+
+        public string CategoryName
+        {
+            get { return _product.Category?.CategoryName ?? ""; }
+        }
+
+        public string ContactName { get { return _product.Supplier?.ContactName ?? ""; } }
+
+        
+    }
+}
---
+++ examjanvier2023/question1/question1/question1/ViewModels/ProductVM.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using question1.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApplication1.ViewModels;
+
+namespace question1.ViewModels
+{
+    internal class ProductVM
+    {
+        private NorthwindContext context = new NorthwindContext();
+
+        private ProductModel _selectedProduct;
+        private DelegateCommand _abortProduct;
+        private ObservableCollection<ProductModel> _ProductsList;
+
+
+        public ObservableCollection<ProductModel> ProductsList { get { return _ProductsList = _ProductsList ?? loadProductList(); } }
+
+        private ObservableCollection<ProductModel> loadProductList()
+        {
+            ObservableCollection<ProductModel> localCollection = new ObservableCollection<ProductModel>();
+            foreach (var item in context.Products)
+            {
+                if (item.Discontinued != true )
+                {
+                    localCollection.Add(new ProductModel(item));
+                }
+            }
+
+            return localCollection;
+        }
+
+
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set { _selectedProduct = value; }
+
+        }
+
+        public DelegateCommand AbortProduct
+        {
+            get { return _abortProduct = _abortProduct ?? new DelegateCommand(abortProduct); }
+        }
+
+        private void abortProduct()
+        {
+            var productToRemove = context.Products.FirstOrDefault(p => p.ProductId == SelectedProduct.ProductId);
+            if (productToRemove != null)
+            {
+                productToRemove.Discontinued = true;
+                _ProductsList.Remove(_selectedProduct);
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+}
